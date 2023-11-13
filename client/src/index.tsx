703850ba78c9d0c8f55451bdcb954f722d3678e9
cdennis-nova <cdennis@nova-compression.com>
2023-11-13 16:14:13 -0600

    Create placeholder unit status page to be filled with content

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Auth0Provider } from '@auth0/auth0-react'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     }}
   >
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </React.StrictMode>
   </Auth0Provider>
 )
---
+++ client/src/pages/units.tsx
@@ -0,0 +1,9 @@
+function Units() {
+    return (
+        <>
+            <h2>Unit Status Page</h2>
+        </>
+    )
+}
+
+export default Units

adad9fcdb49587e2e373091edb10b57aa01dfcc8
cdennis-nova <cdennis@nova-compression.com>
2023-11-20 13:49:14 -0600

    KEP-23 Route for fetching unit params by unit number

--- server/src/routes/parameter.ts
+++ server/src/routes/parameter.ts
@@ -2,11 +2,22 @@ import { FastifyInstance, FastifyRequest } from "fastify"
 import { prisma } from "../prisma-client"
 
 async function routes(fastify: FastifyInstance) {
-    // Default route
+    // Get all unit parameters
     fastify.get("/", async (req, res) => {
         const allParameters = await prisma.parameter.findMany()
 
         return allParameters
+    })
+
+    // Get all parameters for a single unit by unit number
+    fastify.get<{ Params: { unitNum: string } }>("/:unitNum", async (req, res) => {
+        const unitParameters = await prisma.parameter.findMany({
+            where: {
+                unitNumber: req.params.unitNum
+            }
+        })
+
+        return unitParameters
     })
 }
 
--- server/src/routes/unit.ts
+++ server/src/routes/unit.ts
@@ -2,11 +2,22 @@ import { FastifyInstance, FastifyRequest } from "fastify"
 import { prisma } from "../prisma-client"
 
 async function routes(fastify: FastifyInstance) {
-    // Default route
+    // Get all units
     fastify.get("/", async (req, res) => {
         const allUnits = await prisma.unit.findMany()
 
         return allUnits
+    })
+
+    // Get single unit by unit number
+    fastify.get<{ Params: { unitNum: string } }>("/:unitNum", async (req, res) => {
+        const unit = await prisma.unit.findUnique({
+            where: {
+                unitNumber: req.params.unitNum
+            }
+        })
+
+        return unit
     })
 }
 

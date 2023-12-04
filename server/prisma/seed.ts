f6bad8c0f38b445df4e5dbdac4fd97c8972d5756
cdennis-nova <cdennis@nova-compression.com>
2023-12-04 13:33:58 -0600

    Working db seeding

---
+++ server/prisma/seed.ts
@@ -0,0 +1,262 @@
+import * as fs from "fs"
+import * as path from "path"
+import { parse } from "csv-parse"
+import { Unit, Parameter, WeeklyDowntime } from "@prisma/client"
+import { prisma } from "../src/prisma-client"
+
+const NUM_COLS = [
+    "oemHP",
+    "novaHP",
+    "engineRPM",
+    "frameRPM",
+    "monthlyRate",
+    "recurringDiscount",
+    "salesTax",
+    "adValoremHeit",
+    "netRevenue",
+    "ma",
+    "dtHours"
+]
+const DATE_COLS = [
+    "packageMfgDate",
+    "engineMfgDate",
+    "zeroHourDate",
+    "compressorFrameRebuild",
+    "topEnd",
+    "unitSetDate",
+    "billingStartDate",
+    "billingEndDate",
+    "lastBillingDate",
+    "acqEffDate",
+    "unitProfileUpdateDate",
+    "timestamp"
+]
+
+async function main() {
+    /*
+     * Seed Unit table
+     */
+
+    console.log("Seeding Unit Table")
+    var csvFilePath = path.resolve(__dirname, "seeds/unit.csv")
+
+    var headers = [
+        "status",
+        "unitNumber",
+        "customer",
+        "location",
+        "county",
+        "state",
+        "engine",
+        "engineFamily",
+        "engineSerialNum",
+        "afr",
+        "catalyst",
+        "catalystMM",
+        "oemHP",
+        "novaHP",
+        "engineRPM",
+        "frameRPM",
+        "compressorFrame",
+        "compressorFrameFamily",
+        "compressorFrameSN",
+        "stages",
+        "cylinderSize",
+        "packageMfgDate",
+        "engineMfgDate",
+        "zeroHourDate",
+        "compressorFrameRebuild",
+        "topEnd",
+        "oilProvider",
+        "telemetry",
+        "coordinates",
+        "trailerUnit",
+        "unitSetDate",
+        "billingStartDate",
+        "billingEndDate",
+        "lastBillingDate",
+        "onOffContract",
+        "mtmLastPriceIncrease",
+        "salesperson",
+        "contractTypeTax",
+        "contractTypeOil",
+        "monthlyRate",
+        "recurringDiscount",
+        "salesTax",
+        "adValoremHeit",
+        "netRevenue",
+        "dayRate",
+        "origin",
+        "acqEffDate",
+        "ownedLeased",
+        "operationalRegion",
+        "financeRegion",
+        "unitProfileUpdateDate",
+        "financialSku",
+        "oemHPTranche",
+        "bdComments",
+        "opsComments",
+        "setWeek",
+        "releaseWeek",
+    ]
+
+    var fileContent = fs.readFileSync(csvFilePath, { encoding: "utf-8" })
+
+    parse(fileContent, {
+        delimiter: ",",
+        columns: headers,
+        from: 2,
+        cast: (value, context) => {
+            if (value !== "NULL") {
+                if (NUM_COLS.includes(context.column as string)) {
+                    return Number(value)
+                } else if (DATE_COLS.includes(context.column as string)) {
+                    return new Date(value)
+                } else {
+                    return value
+                }
+            } else {
+                return null
+            }
+        }
+    }, async (error, result: Unit[]) => {
+        if (error) {
+            console.error(error)
+        }
+
+        const json = JSON.stringify(result)
+        fs.writeFile('unit.json', json, "utf-8", () => {
+
+        })
+
+        for (const unit of result) {
+            const createdUnit = await prisma.unit.upsert({
+                where: { unitNumber: unit.unitNumber },
+                update: {},
+                create: unit
+            })
+        }
+    })
+
+    /*
+     * Seed Parameter table
+     */
+
+    console.log("Seeding Parameter Table")
+    csvFilePath = path.resolve(__dirname, "seeds/parameter.csv")
+
+    headers = [
+        "unitNumber",
+        "name",
+        "value",
+        "timestamp"
+    ]
+
+    fileContent = fs.readFileSync(csvFilePath, { encoding: "utf-8" })
+
+    parse(fileContent, {
+        delimiter: ",",
+        columns: headers,
+        from: 2,
+        cast: (value, context) => {
+            if (value !== "NULL") {
+                if (NUM_COLS.includes(context.column as string)) {
+                    return Number(value)
+                } else if (DATE_COLS.includes(context.column as string)) {
+                    return new Date(value)
+                } else {
+                    return value
+                }
+            } else {
+                return null
+            }
+        }
+    }, async (error, result: Parameter[]) => {
+        if (error) {
+            console.error(error)
+        }
+
+        const json = JSON.stringify(result)
+        fs.writeFile('parameter.json', json, "utf-8", () => {
+
+        })
+
+        for (const parameter of result) {
+            const createdParameter = await prisma.parameter.upsert({
+                where: {
+                    unitNumber_name: {
+                        unitNumber: parameter.unitNumber,
+                        name: parameter.name
+                    }
+                },
+                update: {},
+                create: parameter
+            })
+        }
+    })
+
+    /*
+     * Seed Weekly Downtime table
+     */
+
+    console.log("Seeding Weekly Downtime Table")
+    csvFilePath = path.resolve(__dirname, "seeds/weekly_downtime.csv")
+
+    headers = [
+        "unitNumber",
+        "week",
+        "ma",
+        "dtHours"
+    ]
+
+    fileContent = fs.readFileSync(csvFilePath, { encoding: "utf-8" })
+
+    parse(fileContent, {
+        delimiter: ",",
+        columns: headers,
+        from: 2,
+        cast: (value, context) => {
+            if (value !== "NULL") {
+                if (NUM_COLS.includes(context.column as string)) {
+                    return Number(value)
+                } else if (DATE_COLS.includes(context.column as string)) {
+                    return new Date(value)
+                } else {
+                    return value
+                }
+            } else {
+                return null
+            }
+        }
+    }, async (error, result: WeeklyDowntime[]) => {
+        if (error) {
+            console.error(error)
+        }
+
+        const json = JSON.stringify(result)
+        fs.writeFile('downtime.json', json, "utf-8", () => {
+
+        })
+
+        for (const weeklyDowntime of result) {
+            const createdWeeklyDowntime = await prisma.weeklyDowntime.upsert({
+                where: {
+                    unitNumber_week: {
+                        unitNumber: weeklyDowntime.unitNumber,
+                        week: weeklyDowntime.week
+                    }
+                },
+                update: {},
+                create: weeklyDowntime
+            })
+        }
+    })
+}
+
+main().then(async () => {
+    await prisma.$disconnect()
+}).catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+})

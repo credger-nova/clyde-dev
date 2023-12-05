77196b83eb4ad870cac0fe2dc07b85b225c3daa0
cdennis-nova <cdennis@nova-compression.com>
2023-12-05 16:10:26 -0600

    Create custom loader

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -1,5 +1,47 @@
+import CircularProgress, { circularProgressClasses, CircularProgressProps } from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+function NovaCircularLoad(props: CircularProgressProps) {
+    return (
+        <Box>
+            <CircularProgress
+                variant="determinate"
+                sx={{
+                    color: (theme) =>
+                        theme.palette.grey[800],
+                    position: "fixed"
+                }}
+                size={60}
+                thickness={6}
+                {...props}
+                value={100}
+            />
+            <CircularProgress
+                variant="indeterminate"
+                disableShrink
+                sx={{
+                    color: "white",
+                    animationDuration: '1000ms',
+                    left: 0,
+                    [`& .${circularProgressClasses.circle}`]: {
+                        strokeLinecap: "round",
+                    },
+                }}
+                size={60}
+                thickness={6}
+                {...props}
+            />
+        </Box>
+    )
+}
+
 export default function Loader() {
     return (
-        <h2>Loading</h2>
+        <div style={{ display: "flex", alignItems: "center", width: "100%", justifyContent: "center" }}>
+            <p style={{ fontSize: "4em" }}>N</p>
+            <NovaCircularLoad />
+            <p style={{ fontSize: "4em" }}>V</p>
+            <p style={{ fontSize: "4em" }}>A</p>
+        </div>
     )
 }

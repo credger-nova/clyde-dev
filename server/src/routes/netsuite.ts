e6b34a692768b6250d4e845a732a57d7b5615633
Carter Dennis <cdennis@nova-compression.com>
2023-12-13 17:40:07 -0600

    Add route placeholder for trucks, update to have a parent route for all NS interactions

--- server/src/routes/netsuite.ts
+++ server/src/routes/netsuite.ts
@@ -3,10 +3,26 @@ import getNsAccessJwt from "../utils/netsuite/get-ns-access-jwt";
 import axios from "axios";
 
 async function routes(fastify: FastifyInstance) {
-    fastify.get("/", async (req, res) => {
+    // Route to get all items
+    fastify.get("/items", async (req, res) => {
         const jwt = await getNsAccessJwt()
 
         const { data } = await axios.get("https://8898907-sb1.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=671&deploy=1",
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${jwt.access_token}`
+                }
+            })
+
+        return data
+    })
+
+    // Route to get trucks
+    fastify.get("/trucks", async (req, res) => {
+        const jwt = await getNsAccessJwt()
+
+        const { data } = await axios.get("",
             {
                 headers: {
                     "Content-Type": "application/json",

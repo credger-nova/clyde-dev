16ee58f74e6acf7742be6625b662df8ebcd867dd
Carter Dennis <cdennis@nova-compression.com>
2023-12-20 17:03:16 -0600

    Add route to get employees from KPA

--- server/src/routes/kpa.ts
+++ server/src/routes/kpa.ts
@@ -5,7 +5,9 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const KPA_KEY = process.env.KPA_KEY
-const URL = "https://api.kpaehs.com/v1/responses.flat"
+const RESPONSES_URL = "https://api.kpaehs.com/v1/responses.flat"
+const USERS_URL = "https://api.kpaehs.com/v1/users.list"
+const JOB_TITLES_URL = "https://api.kpaehs.com/v1/jobtitles.list"
 const LIMIT = 100
 
 async function getPages(): Promise<number> {
@@ -16,9 +18,25 @@ async function getPages(): Promise<number> {
         skip_field_id_mapping_json: true
     }
 
-    const { data } = await axios.post(URL, postBody)
+    const { data } = await axios.post(RESPONSES_URL, postBody)
 
     return data.paging.last_page
+}
+
+async function getJobTitles(): Promise<Array<{ id: string, title: string }>> {
+    var titles = []
+
+    const postBody = {
+        token: KPA_KEY
+    }
+
+    const { data } = await axios.post(JOB_TITLES_URL, postBody)
+
+    for (const title of data.jobtitles) {
+        titles.push({ id: title.id, title: title.title })
+    }
+
+    return titles
 }
 
 async function routes(fastify: FastifyInstance) {
@@ -28,13 +46,13 @@ async function routes(fastify: FastifyInstance) {
         const pages = await getPages()
 
         for (var i = 1; i < pages + 1; i++) {
-            const { data } = await axios.post("https://api.kpaehs.com/v1/responses.flat", {
+            const { data } = await axios.post(RESPONSES_URL, {
                 token: process.env.KPA_KEY,
                 form_id: 217334,
                 limit: LIMIT,
                 skip_field_id_mapping_json: true,
                 columns: [
-                    "og42wkqwer5ufiwx" // AFE #
+                    "og42wkqwer5ufiwx" // AFE Number
                 ],
                 page: i
             })
@@ -47,6 +65,47 @@ async function routes(fastify: FastifyInstance) {
         afeNums = afeNums.filter(value => /^-?\d+\.?\d*$/.test(value))
 
         return afeNums
+    })
+
+    // Route to get Employees
+    fastify.get("/employees", async (req, res) => {
+        var employees = []
+
+        const titles = await getJobTitles()
+
+        const postBody = {
+            token: KPA_KEY
+        }
+
+        const { data } = await axios.post(USERS_URL, postBody)
+
+        for (const user of data.users) {
+            employees.push({
+                id: user.id,
+                firstName: user.firstname,
+                lastName: user.lastname,
+                email: user.email,
+                title: user.jobTitle_id,
+                supervisorId: user.supervisor_id,
+                managerId: user.managerId
+            })
+        }
+
+        for (const employee of employees) {
+            const res = titles.find(obj => obj.id === employee.title)
+
+            // Remove employees with no title
+            if (res) {
+                employee.title = res.title
+            } else {
+                const index = employees.indexOf(employee)
+                if (index > -1) {
+                    employees.splice(index, 1)
+                }
+            }
+        }
+
+        return employees
     })
 }
 

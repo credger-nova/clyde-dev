f97535ec362b49a32b916b8c059bcbfc217e13b1
cdennis-nova <cdennis@nova-compression.com>
2024-05-13 10:47:12 -0500

    Fix indent size

--- server/src/utils/netsuite/sign-ns-jwt.ts
+++ server/src/utils/netsuite/sign-ns-jwt.ts
@@ -5,35 +5,35 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default async function signNsJwt() {
-  var jwtHeader = {
-    alg: "PS256",
-    typ: "JWT",
-    kid: process.env.NS_CERT_ID // Certificate ID on the client credentials mapping
-  };
+    var jwtHeader = {
+        alg: "PS256",
+        typ: "JWT",
+        kid: process.env.NS_CERT_ID // Certificate ID on the client credentials mapping
+    };
 
-  let stringifiedJwtHeader = JSON.stringify(jwtHeader);
+    let stringifiedJwtHeader = JSON.stringify(jwtHeader);
 
-  var jwtPayload = {
-    iss: process.env.NS_CONSUMER_KEY,
-    scope: ["restlets", "rest_webservices"],
-    iat: Date.now() / 1000,
-    exp: Date.now() / 1000 + 3600,
-    aud: process.env.NS_AUDIENCE
-  };
+    var jwtPayload = {
+        iss: process.env.NS_CONSUMER_KEY,
+        scope: ["restlets", "rest_webservices"],
+        iat: Date.now() / 1000,
+        exp: Date.now() / 1000 + 3600,
+        aud: process.env.NS_AUDIENCE
+    };
 
-  let stringifiedJwtPayload = JSON.stringify(jwtPayload);
+    let stringifiedJwtPayload = JSON.stringify(jwtPayload);
 
-  let secret = process.env.NS_CERT_PRIVATE_KEY
-  let encodedSecret = CryptoJS.enc.Base64.stringify(
-    CryptoJS.enc.Utf8.parse(secret!)
-  );
+    let secret = process.env.NS_CERT_PRIVATE_KEY
+    let encodedSecret = CryptoJS.enc.Base64.stringify(
+        CryptoJS.enc.Utf8.parse(secret!)
+    );
 
-  let signedJWT = KJUR.jws.JWS.sign(
-    "PS256",
-    stringifiedJwtHeader,
-    stringifiedJwtPayload,
-    secret
-  );
+    let signedJWT = KJUR.jws.JWS.sign(
+        "PS256",
+        stringifiedJwtHeader,
+        stringifiedJwtPayload,
+        secret
+    );
 
-  return signedJWT
+    return signedJWT
 }

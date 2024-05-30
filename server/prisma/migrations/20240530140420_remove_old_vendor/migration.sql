8e875e7eacfb6443f981c68fc3aaa5a554b39e4d
cdennis-nova <cdennis@nova-compression.com>
2024-05-30 09:09:42 -0500

    KEP-256 - Remove old vendor column

---
+++ server/prisma/migrations/20240530140420_remove_old_vendor/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `vendor` on the `parts_req` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "parts_req" DROP COLUMN "vendor";

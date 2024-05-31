5317881b6623299f6d69137d082bb62c8ebcb9ff
cdennis-nova <cdennis@nova-compression.com>
2024-05-31 12:33:00 -0500

    More robust seeding with data for all necessary tables

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -13,7 +13,8 @@ const NUM_COLS = [
     "adValoremHeit",
     "netRevenue",
     "ma",
-    "dtHours"
+    "dtHours",
+    "cost",
 ]
 const DATE_COLS = [
     "packageMfgDate",
@@ -27,8 +28,337 @@ const DATE_COLS = [
     "lastBillingDate",
     "acqEffDate",
     "unitProfileUpdateDate",
-    "timestamp"
+    "timestamp",
+    "terminationDate",
 ]
+
+const BOOLEAN_COLS = ["active"]
+
+const seedAfe = async () => {
+    console.log("Seeding AFE Table")
+
+    const headers = ["id", "number", "unitNumber", "amount"]
+
+    const parser = fs.createReadStream(`${__dirname}/seeds/afe.csv`).pipe(
+        parse({
+            delimiter: ",",
+            columns: headers,
+            from: 2,
+            cast: (value, context) => {
+                if (value !== "NULL") {
+                    if (NUM_COLS.includes(context.column as string)) {
+                        return Number(value)
+                    } else if (DATE_COLS.includes(context.column as string)) {
+                        return new Date(value)
+                    } else {
+                        return value
+                    }
+                } else {
+                    return null
+                }
+            },
+        })
+    )
+
+    for await (const afe of parser) {
+        await prisma.aFE.upsert({
+            where: { id: afe.id },
+            update: afe,
+            create: afe,
+        })
+    }
+
+    console.log("AFE Table Seeding Complete")
+}
+
+const seedLocation = async () => {
+    console.log("Seeding Location Table")
+
+    const headers = ["id", "name"]
+
+    const parser = fs.createReadStream(`${__dirname}/seeds/location.csv`).pipe(
+        parse({
+            delimiter: ",",
+            columns: headers,
+            from: 2,
+            cast: (value, context) => {
+                if (value !== "NULL") {
+                    if (NUM_COLS.includes(context.column as string)) {
+                        return Number(value)
+                    } else if (DATE_COLS.includes(context.column as string)) {
+                        return new Date(value)
+                    } else {
+                        return value
+                    }
+                } else {
+                    return null
+                }
+            },
+        })
+    )
+
+    for await (const location of parser) {
+        await prisma.location.upsert({
+            where: { id: location.id },
+            update: location,
+            create: location,
+        })
+    }
+
+    console.log("Location Table Seeding Complete")
+}
+
+const seedPart = async () => {
+    console.log("Seeding Part Table")
+
+    const headers = ["id", "itemNumber", "description", "cost", "mode", "type", "active"]
+
+    const parser = fs.createReadStream(`${__dirname}/seeds/part.csv`).pipe(
+        parse({
+            delimiter: ",",
+            columns: headers,
+            from: 2,
+            cast: (value, context) => {
+                if (value !== "NULL") {
+                    if (NUM_COLS.includes(context.column as string)) {
+                        return Number(value)
+                    } else if (DATE_COLS.includes(context.column as string)) {
+                        return new Date(value)
+                    } else if (BOOLEAN_COLS.includes(context.column as string)) {
+                        return value === "True" ? true : false
+                    } else {
+                        return value
+                    }
+                } else {
+                    return null
+                }
+            },
+        })
+    )
+
+    for await (const part of parser) {
+        await prisma.part.upsert({
+            where: { id: part.id },
+            update: part,
+            create: part,
+        })
+    }
+
+    console.log("Part Table Seeding Complete")
+}
+
+const seedSalesOrder = async () => {
+    console.log("Seeding Sales Order Table")
+
+    const headers = ["id", "number", "status"]
+
+    const parser = fs.createReadStream(`${__dirname}/seeds/sales_order.csv`).pipe(
+        parse({
+            delimiter: ",",
+            columns: headers,
+            from: 2,
+            cast: (value, context) => {
+                if (value !== "NULL") {
+                    if (NUM_COLS.includes(context.column as string)) {
+                        return Number(value)
+                    } else if (DATE_COLS.includes(context.column as string)) {
+                        return new Date(value)
+                    } else {
+                        return value
+                    }
+                } else {
+                    return null
+                }
+            },
+        })
+    )
+
+    for await (const salesOrder of parser) {
+        await prisma.salesOrder.upsert({
+            where: { id: salesOrder.id },
+            update: salesOrder,
+            create: salesOrder,
+        })
+    }
+
+    console.log("Sales Order Table Seeding Complete")
+}
+
+const seedTruck = async () => {
+    console.log("Seeding Truck Table")
+
+    const headers = ["id", "name", "altName"]
+
+    const parser = fs.createReadStream(`${__dirname}/seeds/truck.csv`).pipe(
+        parse({
+            delimiter: ",",
+            columns: headers,
+            from: 2,
+            cast: (value, context) => {
+                if (value !== "NULL") {
+                    if (NUM_COLS.includes(context.column as string)) {
+                        return Number(value)
+                    } else if (DATE_COLS.includes(context.column as string)) {
+                        return new Date(value)
+                    } else {
+                        return value
+                    }
+                } else {
+                    return null
+                }
+            },
+        })
+    )
+
+    for await (const truck of parser) {
+        await prisma.truck.upsert({
+            where: { id: truck.id },
+            update: truck,
+            create: truck,
+        })
+    }
+
+    console.log("Truck Table Seeding Complete")
+}
+
+const seedUser = async () => {
+    console.log("Seeding User Table")
+
+    const headers = [
+        "id",
+        "firstName",
+        "lastName",
+        "email",
+        "cellPhone",
+        "jobTitle",
+        "region",
+        "managerId",
+        "supervisorId",
+        "terminationDate",
+    ]
+
+    const parser = fs.createReadStream(`${__dirname}/seeds/user.csv`).pipe(
+        parse({
+            delimiter: ",",
+            columns: headers,
+            from: 2,
+            cast: (value, context) => {
+                if (value !== "NULL") {
+                    if (NUM_COLS.includes(context.column as string)) {
+                        return Number(value)
+                    } else if (DATE_COLS.includes(context.column as string)) {
+                        return new Date(value)
+                    } else {
+                        return value
+                    }
+                } else {
+                    return null
+                }
+            },
+        })
+    )
+
+    // Initial upsert of users (without manager or supervisor id)
+    for await (const user of parser) {
+        await prisma.user.upsert({
+            where: { id: user.id },
+            update: {
+                id: user.id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+                cellPhone: user.cellPhone,
+                jobTitle: user.jobTitle,
+                region: user.region,
+                terminationDate: user.terminationDate,
+            },
+            create: {
+                id: user.id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+                cellPhone: user.cellPhone,
+                jobTitle: user.jobTitle,
+                region: user.region,
+                terminationDate: user.terminationDate,
+            },
+        })
+    }
+
+    // Add manager and supervisor IDs
+    const secondParser = fs.createReadStream(`${__dirname}/seeds/user.csv`).pipe(
+        parse({
+            delimiter: ",",
+            columns: headers,
+            from: 2,
+            cast: (value, context) => {
+                if (value !== "NULL") {
+                    if (NUM_COLS.includes(context.column as string)) {
+                        return Number(value)
+                    } else if (DATE_COLS.includes(context.column as string)) {
+                        return new Date(value)
+                    } else {
+                        return value
+                    }
+                } else {
+                    return null
+                }
+            },
+        })
+    )
+
+    for await (const user of secondParser) {
+        await prisma.user.upsert({
+            where: { id: user.id },
+            update: {
+                managerId: user.managerId,
+                supervisorId: user.supervisorId,
+            },
+            create: user,
+        })
+    }
+
+    console.log("User Table Seeding Complete")
+}
+
+const seedVendor = async () => {
+    console.log("Seeding Vendor Table")
+
+    const headers = ["id", "name", "active"]
+
+    const parser = fs.createReadStream(`${__dirname}/seeds/vendor.csv`).pipe(
+        parse({
+            delimiter: ",",
+            columns: headers,
+            from: 2,
+            cast: (value, context) => {
+                if (value !== "NULL") {
+                    if (NUM_COLS.includes(context.column as string)) {
+                        return Number(value)
+                    } else if (DATE_COLS.includes(context.column as string)) {
+                        return new Date(value)
+                    } else if (BOOLEAN_COLS.includes(context.column as string)) {
+                        return value === "True" ? true : false
+                    } else {
+                        return value
+                    }
+                } else {
+                    return null
+                }
+            },
+        })
+    )
+
+    for await (const vendor of parser) {
+        await prisma.vendor.upsert({
+            where: { id: vendor.id },
+            update: vendor,
+            create: vendor,
+        })
+    }
+
+    console.log("Vendor Table Seeding Complete")
+}
 
 const seedUnit = async () => {
     console.log("Seeding Unit Table")
@@ -93,12 +423,11 @@ const seedUnit = async () => {
         "releaseWeek",
         "assignedTechnician",
         "assignedManager",
-        "assignedDirector"
+        "assignedDirector",
     ]
 
-    const parser = fs
-        .createReadStream(`${__dirname}/seeds/unit.csv`)
-        .pipe(parse({
+    const parser = fs.createReadStream(`${__dirname}/seeds/unit.csv`).pipe(
+        parse({
             delimiter: ",",
             columns: headers,
             from: 2,
@@ -114,14 +443,15 @@ const seedUnit = async () => {
                 } else {
                     return null
                 }
-            }
-        }))
+            },
+        })
+    )
 
     for await (const unit of parser) {
         await prisma.unit.upsert({
             where: { unitNumber: unit.unitNumber },
             update: unit,
-            create: unit
+            create: unit,
         })
     }
 
@@ -131,16 +461,10 @@ const seedUnit = async () => {
 const seedParameter = async () => {
     console.log("Seeding Parameter Table")
 
-    const headers = [
-        "unitNumber",
-        "name",
-        "value",
-        "timestamp"
-    ]
+    const headers = ["unitNumber", "name", "value", "timestamp"]
 
-    const parser = fs
-        .createReadStream(`${__dirname}/seeds/parameter.csv`)
-        .pipe(parse({
+    const parser = fs.createReadStream(`${__dirname}/seeds/parameter.csv`).pipe(
+        parse({
             delimiter: ",",
             columns: headers,
             from: 2,
@@ -156,19 +480,20 @@ const seedParameter = async () => {
                 } else {
                     return null
                 }
-            }
-        }))
+            },
+        })
+    )
 
     for await (const parameter of parser) {
         await prisma.parameter.upsert({
             where: {
                 unitNumber_name: {
                     unitNumber: parameter.unitNumber,
-                    name: parameter.name
-                }
+                    name: parameter.name,
+                },
             },
             update: parameter,
-            create: parameter
+            create: parameter,
         })
     }
 
@@ -178,16 +503,10 @@ const seedParameter = async () => {
 const seedWeeklyDowntime = async () => {
     console.log("Seeding Weekly Downtime Table")
 
-    const headers = [
-        "unitNumber",
-        "week",
-        "ma",
-        "dtHours"
-    ]
+    const headers = ["unitNumber", "week", "ma", "dtHours"]
 
-    const parser = fs
-        .createReadStream(`${__dirname}/seeds/weekly_downtime.csv`)
-        .pipe(parse({
+    const parser = fs.createReadStream(`${__dirname}/seeds/weekly_downtime.csv`).pipe(
+        parse({
             delimiter: ",",
             columns: headers,
             from: 2,
@@ -203,19 +522,20 @@ const seedWeeklyDowntime = async () => {
                 } else {
                     return null
                 }
-            }
-        }))
+            },
+        })
+    )
 
     for await (const weeklyDowntime of parser) {
         await prisma.weeklyDowntime.upsert({
             where: {
                 unitNumber_week: {
                     unitNumber: weeklyDowntime.unitNumber,
-                    week: weeklyDowntime.week
-                }
+                    week: weeklyDowntime.week,
+                },
             },
             update: weeklyDowntime,
-            create: weeklyDowntime
+            create: weeklyDowntime,
         })
     }
 
@@ -224,6 +544,15 @@ const seedWeeklyDowntime = async () => {
 
 async function main() {
     await seedUnit()
+
+    await seedAfe()
+    await seedLocation()
+    await seedPart()
+    await seedSalesOrder()
+    await seedTruck()
+    await seedUser()
+    await seedVendor()
+
     await seedParameter()
     await seedWeeklyDowntime()
 
